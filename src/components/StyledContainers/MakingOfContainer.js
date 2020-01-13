a283a0ce62be61a40ee98d1ccc6539ae976e9154
Matthew Mahler <matthewjamesmahler@gmail.com>
2020-01-13 18:06:15 -0500

    im dumb

--- src/components/StyledContainers/MakingOfContainer.js
+++ src/components/StyledContainers/MakingOfContainer.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Container = styled.div`
   position: relative;
   width: 100%;
-  min-height: 250vh;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
